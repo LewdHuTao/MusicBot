54118dd9f0ca53e2d1e2b97a0198589bfe00e554
LewdHuTao <imanimtiyaz242@gmail.com>
2024-06-19 22:44:24 +0800

    feat: add music command

---
+++ src/commands/slash/music/DisconnectSlash.js
@@ -0,0 +1,68 @@
+const SlashCommand = require("../../../structures/SlashCommand");
+const { EmbedBuilder } = require("discord.js");
+
+const command = new SlashCommand()
+  .setName("disconnect")
+  .setDescription("Leaves the voice channel and clears the queue.")
+  .setCategory("Music")
+  .setRun(async (client, interaction, options) => {
+    const player = client.manager.players.get(interaction.guild.id);
+
+    if (!player) {
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor(client.embedColor)
+            .setDescription(`:x: | The queue is empty.`),
+        ],
+        ephemeral: true,
+      });
+    }
+
+    if (!interaction.member.voice.channel) {
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor("Red")
+            .setDescription(
+              `:x: | You need to be in a voice channel to use this command.`
+            ),
+        ],
+        ephemeral: true,
+      });
+    }
+
+    if (
+      interaction.guild.members.me.voice.channel &&
+      !interaction.guild.members.me.voice.channel.equals(
+        interaction.member.voice.channel
+      )
+    ) {
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor("Red")
+            .setDescription(
+              `:x: | You need to be in the same voice channel as the bot to use this command.`
+            ),
+        ],
+        ephemeral: true,
+      });
+    }
+
+    await interaction.deferReply();
+
+    await player.disconnect();
+
+    return interaction.editReply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor(client.embedColor)
+          .setDescription(
+            `:white_check_mark: | Leaves <#${interaction.guild.members.me.voice.channel.id}> and clears the queue.`
+          ),
+      ],
+    });
+  });
+
+module.exports = command;
---
+++ src/commands/slash/music/ResumeSlash.js
@@ -0,0 +1,82 @@
+const SlashCommand = require("../../../structures/SlashCommand");
+const { EmbedBuilder } = require("discord.js");
+
+const command = new SlashCommand()
+  .setName("resume")
+  .setDescription("Resume current playing track.")
+  .setCategory("Music")
+  .setRun(async (client, interaction, options) => {
+    const player = client.manager.players.get(interaction.guild.id);
+
+    if (!player) {
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor(client.embedColor)
+            .setDescription(`:x: | The queue is empty.`),
+        ],
+        ephemeral: true,
+      });
+    }
+
+    if (!interaction.member.voice.channel) {
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor("Red")
+            .setDescription(
+              `:x: | You need to be in a voice channel to use this command.`
+            ),
+        ],
+        ephemeral: true,
+      });
+    }
+
+    if (
+      interaction.guild.members.me.voice.channel &&
+      !interaction.guild.members.me.voice.channel.equals(
+        interaction.member.voice.channel
+      )
+    ) {
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor("Red")
+            .setDescription(
+              `:x: | You need to be in the same voice channel as the bot to use this command.`
+            ),
+        ],
+        ephemeral: true,
+      });
+    }
+
+    if (!player.paused) {
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor('Red')
+            .setDescription(
+              `:x: | The current track is already resumed.`
+            )
+        ],
+        ephemeral: true,
+      })
+    }
+
+    await interaction.deferReply();
+
+    await player.pause(false);
+    let song = player.current;
+
+    return interaction.editReply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor(client.embedColor)
+          .setDescription(
+            `:white_check_mark: | Resumed playing: [\`${song.info.title}\`](${song.info.uri})`
+          ),
+      ],
+    });
+  });
+
+module.exports = command;
