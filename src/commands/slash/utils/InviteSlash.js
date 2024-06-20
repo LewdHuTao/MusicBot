a3c9bda3c58b60afe3c2cf97acacaa2f406f77d4
LewdHuTao <imanimtiyaz242@gmail.com>
2024-06-20 20:11:15 +0800

    feat: add utils command

---
+++ src/commands/slash/utils/InviteSlash.js
@@ -0,0 +1,15 @@
+const SlashCommand = require("../../../structures/SlashCommand");
+
+const command = new SlashCommand()
+  .setName("invite")
+  .setDescription("Invite me to your server..")
+  .setCategory("Util")
+  .setRun(async (client, interaction, options) => {
+    await interaction.deferReply({ ephemeral: true });
+
+    return interaction.editReply({
+      content: `https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=277062150416&scope=bot%20applications.commands`,
+    });
+  });
+
+module.exports = command;
