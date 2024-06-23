b4dbcac1093f8a52e273052fcaf518d980d0038e
LewdHuTao <imanimtiyaz242@gmail.com>
2024-06-23 22:24:43 +0800

    default config

--- src/config.js
+++ src/config.js
@@ -2,28 +2,31 @@ require("dotenv").config();
 
 module.exports = {
   bot: {
-    token: process.env.token || "",
-    clientName: process.env.clientName || "SomeCoolName",
-    clientId: process.env.clientId || "",
-    clientSecret: process.env.clientSecret || "",
+    token: process.env.token || "", // Bot token
+    clientName: process.env.clientName || "SomeCoolName", // Bot username
+    clientId: process.env.clientId || "", // Bot clientId
+    clientSecret: process.env.clientSecret || "", // Bot clientSecret
   },
 
   owner: {
-    userId: process.env.userId || "",
+    userId: process.env.userId || "", // Owner userId for developer command
   },
 
   botSettings: {
-    mongoUrl: process.env.mongoUrl || "",
-    geniusToken: process.env.geniusToken || "",
-    prefix: process.env.prefix || "?"
+    mongoUrl: process.env.mongoUrl || "", // You can leave it blank, mongo is not setup yet
+    geniusToken: process.env.geniusToken || "", // Use to fetch lyrics, you can leave it blank.
+    prefix: process.env.prefix || "?", // You can leave it blank, prefix function is not setup yet
+    embedColor: process.env.embedColor || "2F3136", // You can use any HEX Color but without the "#"
   },
 
+  // Lavalink settings. Please use lavalink v4
   nodes: [
     {
-
+      name: "node1",
+      host: "localhost",
+      password: "youshallnotpass",
+      port: 2333,
+      secure: false,
     },
-    {
-        
-    }
   ]
 };
