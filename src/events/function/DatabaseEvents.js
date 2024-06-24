3fd4a529ed43b048210e7ce9b86e47e31917d908
LewdHuTao <imanimtiyaz242@gmail.com>
2024-06-24 20:26:04 +0800

    feat: database structure

---
+++ src/events/function/DatabaseEvents.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+module.exports = {
+  name: "ready",
+  run: async (client, message) => {
+    mongoose.connect(client.settings.mongoUrl);
+    mongoose.Promise = global.Promise;
+    mongoose.set("strictQuery", true);
+    mongoose.connection.on("connected", () => {
+      client.bot.info(
+        "Database connection connected. MongoDB is ready.",
+        "ready"
+      );
+    });
+    mongoose.connection.on("err", (err) => {
+      client.bot.error(
+        `Database connection error. MongoDB-01 is not ready.`,
+        "error"
+      );
+    });
+    mongoose.connection.on("disconnected", () => {
+      client.bot.warn(
+        "Database connection disconnecte. MongoDB-01 is disconnected."
+      );
+    });
+  },
+};
