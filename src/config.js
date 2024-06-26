8fc7eaaed248aa7bd1d59d03a7ab50f8d40e33c7
LewdHuTao <imanimtiyaz242@gmail.com>
2024-06-26 22:53:38 +0800

    update .env and config

--- src/config.js
+++ src/config.js
@@ -14,9 +14,9 @@ module.exports = {
   },
 
   botSettings: {
-    mongoUrl: process.env.mongoUrl || "", // You can leave it blank, mongo is not setup yet
-    geniusToken: process.env.geniusToken || "", // Use to fetch lyrics, you can leave it blank.
-    prefix: process.env.prefix || "?", // You can leave it blank, prefix function is not setup yet
+    mongoUrl: process.env.mongoUrl || "", // Mongodb url for database
+    geniusToken: process.env.geniusToken || "", // Genius token use to fetch lyrics, you can leave it blank.
+    prefix: process.env.prefix || "?", // Default prefix is set to "?" use prefix command to change
     embedColor: process.env.embedColor || "2F3136", // You can use any HEX Color but without the "#"
   },
 
