0a8b063a81c97d97e70431eabc8a2a2332fb6b53
LewdHuTao <imanimtiyaz242@gmail.com>
2024-07-04 12:07:23 +0800

    fix: enable ap/247 mode broke the player when disconnect

--- src/events/audio/queueEnd.js
+++ src/events/audio/queueEnd.js
@@ -2,15 +2,32 @@ const { EmbedBuilder } = require("discord.js");
 const PlayerHandler = require("../../structures/PlayerHandler");
 
 module.exports = async (client, player) => {
-  channel = client.channels.cache.get(player.textChannel);
+  const channel = client.channels.cache.get(player.textChannel);
+
+  const QueueEnd = async () => {
+    await player.destroy();
+    client.node.warn(`A player has been destroyed in guild: ${player.guildId}`);
+    return channel.send({
+      embeds: [
+        new EmbedBuilder()
+          .setColor(client.embedColor)
+          .setDescription(
+            `:musical_note: | Destroyed the player and leaves <#${player.voiceChannel}>`
+          ),
+      ],
+    });
+  };
+
+  const m = await PlayerHandler.nowPlayingMessage.fetch().catch(() => {});
+  if (m && m.deletable) m.delete().catch(() => {});
+
+  if (!player.connected) {
+    return await QueueEnd();
+  }
 
   if (player.get("autoplay")) {
-    PlayerHandler.deleteMessageWithRetries(PlayerHandler.nowPlayingMessage);
-
-    return player.autoplay(player);
+    await player.autoplay(player);
   } else if (player.get("stay")) {
-    PlayerHandler.deleteMessageWithRetries(PlayerHandler.nowPlayingMessage);
-
     channel.send({
       embeds: [
         new EmbedBuilder()
@@ -21,19 +38,6 @@ module.exports = async (client, player) => {
       ],
     });
   } else {
-    PlayerHandler.deleteMessageWithRetries(PlayerHandler.nowPlayingMessage);
-
-    await player.destroy();
-    client.node.warn(`A player has been destroyed in guild: ${player.guildId}`);
-
-    return channel.send({
-      embeds: [
-        new EmbedBuilder()
-          .setColor(client.embedColor)
-          .setDescription(
-            `:musical_note: | Destroyed the player and leaves <#${player.voiceChannel}>`
-          ),
-      ],
-    });
+    await QueueEnd();
   }
 };
