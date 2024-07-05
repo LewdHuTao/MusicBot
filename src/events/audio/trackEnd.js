04c83636616795134c8425a038d68b89d71554b8
LewdHuTao <imanimtiyaz242@gmail.com>
2024-07-05 10:17:59 +0800

    feat: move retries delete function to track end event

--- src/events/audio/trackEnd.js
+++ src/events/audio/trackEnd.js
@@ -1,6 +1,18 @@
 const PlayerHandler = require("../../structures/PlayerHandler");
 
 module.exports = async () => {
-  const m = await PlayerHandler.nowPlayingMessage.fetch().catch(() => {});
-  if (m && m.deletable) m.delete().catch(() => {});
+  let retries = 5;
+  let deleteSuccess = false;
+  while (retries > 0 && !deleteSuccess) {
+    try {
+      const m = await PlayerHandler.nowPlayingMessage.fetch();
+      if (m && m.deletable) {
+        await m.delete();
+        deleteSuccess = true;
+      }
+    } catch (error) {
+      client.bot.warn(`Error deleting message: ${error.message}`);
+    }
+    retries--;
+  }
 };
